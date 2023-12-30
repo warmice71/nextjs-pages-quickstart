22b3fea9e960e8fdaa9c37539dbb121546aa5322
warmice71 <awl19950228@gmail.com>
2023-12-30 01:55:21 +0100

    nextjs-pages-quickstart

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <UserButton afterSignOutUrl="/" />
+      </header>
+      <div>Your page's content can go here.</div>
+      <div>Your page's content can go here.</div>
+    </>
+  );
+}
+
